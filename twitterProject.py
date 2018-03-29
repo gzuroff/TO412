3d33e0f09d9f0b8680d880de397b888d30069c33
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2018-03-28 21:06:36 -0400

    Added a proof of concept linear regression model to see significance of variable

--- twitterProject.py
+++ twitterProject.py
@@ -1,6 +1,7 @@
 import tweepy
 import pandas as pd
 import numpy as np
+import statsmodels.formula.api as smf
 
 
 key = "Pfv5AqNYvf2dpTAqOiJLRXbsk"
@@ -20,14 +21,29 @@ for comp in comps:
             #print(tweet.text.rstrip().replace("\n", "").replace(",","").replace("\r",""))
             df = df.append({
                 "text": tweet.text.rstrip().replace("\n", "").replace(",","").replace("\r",""),
-                "created": tweet.created_at,
+                "created": tweet.created_at.hour,
                 "re": tweet.retweet_count,
                 "fav": tweet.favorite_count,
                 "followC": tweet.user.followers_count,
                 "sn": tweet.user.screen_name
             }, ignore_index=True)
-
+    #basic linear regression model
+    #TODO: sentiment score with neural net
+    df.loc[:,"sn"] = pd.Categorical(df.sn).codes
+    df = df.loc[:,df.columns != "text"]
     tweets[comp] = df
+    features = tweets[comp].loc[:, ["created", "re", "followC"]]
+    target = tweets[comp].loc[:, df.columns == 'fav']
+    model = smf.ols(formula = 'fav ~ created + re + followC', data = df.astype(float)).fit()
+    predictions = model.predict(features.astype(float))
+    print(model.summary())
+
+# for comp in comps:
+#     features = tweets[comp].loc[:, df.columns != 'fav']
+#     target = tweets[comp].loc[:, df.column == 'fav']
+#     sm.OLS(target, features).fit()
+#     predictions = model.predict(features)
+#     model.summary()
 
 #print(tweets["wendys"].to_string())
 
