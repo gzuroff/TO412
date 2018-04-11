9d4fd3783058b8d38068ee43bd9766150b7f2ad8
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2018-04-11 14:04:57 -0400

    Added in Google trends data

--- google.py
+++ google.py
@@ -3,12 +3,19 @@ import pandas as pd
 import datetime as dt
 
 pytrends = TrendReq(hl='en-US', tz=360)
-kw_list = ["Wendys", "Arbys", "McDonalds", "Burger King"]
+kw_list = ["Wendys", "Burger King", "McDonalds", "Arbys"]
 
 
-pytrends.build_payload(kw_list, cat=0, timeframe='today 2-y', geo='', gprop='')
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
 trends = pytrends.interest_over_time()
 days = pd.to_datetime(trends.index).to_series()#.apply(lambda x: dt.datetime.strftime(x, '%m/%d/%Y'))
 trends["week"] = days.dt.week
 trends["year"] = days.dt.year
-print(trends)
+comps = ["wendys", "burgerking", "mcdonalds", "arbys"]
+for i in range(len(comps)):
+    filename = comps[i] + ".csv"
+    origFile = pd.read_csv(filename)
+    compTrends = trends.ix[:,[kw_list[i], "week", "year"]]
+    newFile = pd.merge(origFile, compTrends,  how='inner', left_on=['week','year'], right_on = ['week','year'])
+    f_out = comps[i] + "1.csv"
+    newFile.to_csv(f_out)
