475a11bbf42ec9b2d5f62c6665d3b418063670bc
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2018-04-15 15:56:25 -0400

    fixes

--- google.py
+++ google.py
@@ -4,18 +4,16 @@ import datetime as dt
 
 pytrends = TrendReq(hl='en-US', tz=360)
 kw_list = ["Wendys", "Burger King", "McDonalds", "Arbys"]
-
-
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-trends = pytrends.interest_over_time()
-days = pd.to_datetime(trends.index).to_series()#.apply(lambda x: dt.datetime.strftime(x, '%m/%d/%Y'))
-trends["week"] = days.dt.week
-trends["year"] = days.dt.year
 comps = ["wendys", "burgerking", "mcdonalds", "arbys"]
-for i in range(len(comps)):
-    filename = comps[i] + ".csv"
+for j in range(len(kw_list)):
+    pytrends.build_payload([kw_list[j]], cat=0, timeframe='today 5-y', geo='', gprop='')
+    trends = pytrends.interest_over_time()
+    days = pd.to_datetime(trends.index).to_series()
+    trends["week"] = days.dt.week
+    trends["year"] = days.dt.year
+    filename = comps[j] + ".csv"
     origFile = pd.read_csv(filename)
-    compTrends = trends.ix[:,[kw_list[i], "week", "year"]]
+    compTrends = trends.ix[:,[kw_list[j], "week", "year"]]
     newFile = pd.merge(origFile, compTrends,  how='inner', left_on=['week','year'], right_on = ['week','year'])
-    f_out = comps[i] + "1.csv"
+    f_out = comps[j] + "1.csv"
     newFile.to_csv(f_out)
